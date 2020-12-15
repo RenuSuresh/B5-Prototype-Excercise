913dc9a9bcc40d251d99789b201cf2582359872a
RenuSuresh <34194473+RenuSuresh@users.noreply.github.com>
2020-12-15 23:47:57 +0530

    updated Changes

--- index.js
+++ index.js
@@ -37,10 +37,24 @@ function Airplane(name) {
           + It should return a string with `name` and `age`. Example: "Mary, 50"
   */
   
- function Person() {
-    
-  }
- 
+function Person(name,age) {
+  this.name = name;
+    this.age = age;
+    this.stomach = [];
+}
+
+Person.prototype.eat = function(somefood) {
+if(this.stomach.length<10){
+return this.stomach.push(somefood)
+}
+}
+Person.prototype.poop = function(){
+return this.stomach.length = 0
+}
+
+Person.prototype.toString = function(){
+return `${this.name}, ${this.age}`
+}
  
 
   
@@ -61,10 +75,36 @@ function Airplane(name) {
           + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
   */
   
- function Car() {
-    
+
+function Car(model, milesPerGallon) {
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+
+}
+
+Car.prototype.fill = function(gallons){
+return this.tank += gallons;
+}
+
+Car.prototype.drive = function(distance){
+  
+  const distanceCanTravel = this.tank* this.milesPerGallon;
+  if(distance > distanceCanTravel){
+    this.odometer += distanceCanTravel;
+    this.tank = 0;
+    return `I ran out of fuel at  ${this.odometer} miles!"`;
+  } else{
+
+    this.odometer += distance;
+    this.tank -= distance/this.milesPerGallon;
   }
   
+  return (this.odometer, this.tank)
+}
+
+
   
   /*
     TASK 3
@@ -73,16 +113,21 @@ function Airplane(name) {
       - Besides the methods on Person.prototype, babies have the ability to `.play()`:
           + Should return a string "Playing with x", x being the favorite toy.
   */
- function Baby() {
-   
-  }
+ function Baby(favoriteToy) {
+  this.favoriteToy = favoriteToy
+}
+Baby.prototype = Object.create(Person.prototype);
+
+Baby.prototype.play = function(){
+  return `Playing with, ${this.favoriteToy}.`
+}
  
   
   /* 
     TASK 4
     In your own words explain the four principles for the "this" keyword below:
-    1. 
-    2. 
+    1. 'this' can be said, that it is global object
+    2. 'this' is used as a reference
     3. 
     4. 
   */
